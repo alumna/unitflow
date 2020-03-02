0b016d464017c83e8cbe79b4a4184950ab238bd6
Paulo Coghi <paulo@coghi.com.br>
2020-03-02 13:01:37 -0300

    Dependency feature released (#4)
    Dependency feature released

--- test/without-dependencies.test.js
+++ test/without-dependencies.test.js
@@ -3,33 +3,78 @@ import Unitflow from './../src/unitflow';
 
 describe( 'Flows without dependencies', () => {
 
-	test('1. One flow, two units', async ( done ) => {
+	test('1. One flow and two units', async ( done ) => {
 
-			const lib = new Unitflow();
-			
-			expect( lib.state ).toEqual( {} );
+		const lib = new Unitflow();
+		
+		expect( lib.state ).toEqual( {} );
 
-			lib.unit[ 'task 1' ] = function ( state, next ) {
-				state[ 'task 1' ] = 'done'
-				next();
-			}
+		lib.unit[ 'task 1' ] = function ( state, next ) {
+			state[ 'task 1' ] = 'done'
+			next();
+		}
 
-			lib.unit[ 'task 2' ] = function ( state, next ) {
-				state[ 'task 2' ] = 'done'
-				next();
-			}
+		lib.unit[ 'task 2' ] = function ( state, next ) {
+			state[ 'task 2' ] = 'done'
+			next();
+		}
 
-			lib.flow[ 'tasks' ] = [ 'task 1', 'task 2' ]
+		lib.flow[ 'tasks' ] = [ 'task 1', 'task 2' ]
 
-			await lib.run( 'tasks' )
+		await lib.run( 'tasks' )
 
-			expect( lib.state ).toEqual({
-				'task 1': 'done',
-				'task 2': 'done'
-			});
-
-			done();
-
+		expect( lib.state ).toEqual({
+			'task 1': 'done',
+			'task 2': 'done'
 		});
+
+		done();
+
+	});
+
+	test('2. Two flows and two units each', async ( done ) => {
+
+		const lib = new Unitflow();
+		
+		expect( lib.state ).toEqual( {} );
+
+		/* Flow 1 */
+		lib.unit[ 'task 1' ] = function ( state, next ) {
+			state[ 'task 1' ] = 'done'
+			next();
+		}
+
+		lib.unit[ 'task 2' ] = function ( state, next ) {
+			state[ 'task 2' ] = 'done'
+			next();
+		}
+
+		lib.flow[ 'flow 1' ] = [ 'task 1', 'task 2' ]
+
+		/* Flow 2 */
+		lib.unit[ 'task 3' ] = function ( state, next ) {
+			state[ 'task 3' ] = 'done'
+			next();
+		}
+
+		lib.unit[ 'task 4' ] = function ( state, next ) {
+			state[ 'task 4' ] = 'done'
+			next();
+		}
+
+		lib.flow[ 'flow 2' ] = [ 'task 3', 'task 4' ]
+
+		await lib.run( 'flow 1', 'flow 2' )
+
+		expect( lib.state ).toEqual({
+			'task 1': 'done',
+			'task 2': 'done',
+			'task 3': 'done',
+			'task 4': 'done'
+		});
+
+		done();
+
+	});
 
 });
