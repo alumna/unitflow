8c5f6a23d42efacc89a6c110934aad477560777b
Paulo Coghi <paulocoghi@gmail.com>
2020-03-02 14:14:59 -0300

    Dependency feature released (#4)

--- dist/unitflow.cjs.js
+++ dist/unitflow.cjs.js
@@ -10,39 +10,129 @@ class Unitflow {
 
 	}
 
-	link ( flow_units, resolve ) {
+	async run ( ...args ) {
+		
+		const promises = [], run_data = this.prepare( args );
 
-		const 	sequence = {},
-				length   = flow_units.length,
+		run_data.flows.reverse();
+
+		for ( let i = run_data.flows.length - 1; i >= 0; i-- ) {
+			
+			const promise = new Promise( resolve => this.link_and_start( run_data.flows[i], run_data, resolve ) );
+			promises.push( promise );
+		}
+
+		return Promise.all( promises )
+	}
+
+	prepare ( flows ) {
+
+		/* Verify if there are dependencies */
+		let dependencies = flows.pop();
+		const dependents = {};
+
+		if ( typeof dependencies == 'string' ) {
+			flows.push( dependencies );
+			dependencies = {};
+		}
+		else
+			this.dependents( dependencies, dependents );
+
+
+		return { flows, dependencies, dependents, execution: {} }
+
+	}
+
+	dependents ( all_dependencies, dependents ) {
+
+		for ( let [ dependent, local_dependencies ] of Object.entries( all_dependencies ) ) {
+
+			for ( let i = local_dependencies.length - 1; i >= 0; i--) {
+				
+				if ( !dependents[ local_dependencies[i] ] ) dependents[ local_dependencies[i] ] = [];
+
+				dependents[ local_dependencies[i] ].push( dependent );
+			}
+
+		}
+
+	}
+
+	link_and_start ( flow, run_data, resolve ) {
+
+		const	units	 = this.flow[ flow ],
+				length   = units.length,
+				sequence = {},
 				state    = this.state;
+		
 
 		for ( let i = length - 1; i >= 0; i-- ) {
 			
-			const unit = this.unit[ flow_units[i] ];
-			const next = i == ( length - 1 ) ? resolve : sequence[ i + 1 ];
-			const func = async () => unit( state, next );
+			const 	unit = this.unit[ units[i] ].bind( this ),
+					next = i == ( length - 1 ) ? resolve : sequence[ i + 1 ],
+					name = flow + ':' + units[i],
+					func = async () => unit( state, next, resolve );
 
-			sequence[ i ] = func;
+			sequence[ i ] = () => this.wait_or_run( name, func, run_data );
 		}
 
 		sequence[0]();
 
 	}
 
-	async run ( ...args ) {
-		
-		args.reverse();
+	wait_or_run ( name, func, run_data, ready = false ) {
 
-		const promises = [];
+		/* 
+		 * When there are dependencies and they are not finished,
+		 * save the unit's function to be automatically executed
+		 * when the last dependency finishes
+		 */
+		if ( !ready && !this.dependencies_completed( name, run_data ) )
+			return run_data.execution[ name ] = () => this.wait_or_run( name, func, run_data, true );
 
-		for ( let i = args.length - 1; i >= 0; i-- ) {
+		run_data.execution[ name ] = true;
 
-			const promise = new Promise( resolve => this.link( this.flow[ args[i] ], resolve ) );
+		func();
 
-			promises.push( promise );
+		this.check_dependents( name, run_data );
+	}
+
+	dependencies_completed ( name, run_data ) {
+
+		const dependencies = run_data.dependencies[ name ];
+
+		if ( !dependencies )
+			return true;
+
+		return dependencies.every( dep => run_data.execution[ dep ] === true )
+
+	}
+
+	check_dependents ( name, run_data ) {
+
+		const dependents = run_data.dependents[ name ];
+
+		if ( !dependents )
+			return;
+
+		for ( let i = dependents.length - 1; i >= 0; i-- ) {
+			
+			const dependent = dependents[i];
+
+			// If the dependent is not waiting, stop here and
+			// do not check the completition of dependencies,
+			// because the dependent itself will do this
+			if ( typeof run_data.execution[ dependent ] !== 'function' )
+				continue;
+			
+			// Otherwise, if the dependent is waiting, check if
+			// this dependency was the last one to complete.
+			// When it is the case, lets reactively call the
+			// dependent.
+			if ( this.dependencies_completed( dependent, run_data ) )
+				run_data.execution[ dependent ]();
 		}
 
-		return Promise.all( promises )
 	}
 
 }
